94aee0e6ed81ea96dc4d7a41cc059cfe69ba7060
Igor Kurylev <kurilevigor@mail.ru>
2019-10-09 18:29:59 +0300

    upd tests for hw2

--- tests/test_tick_tack_toe.py
+++ tests/test_tick_tack_toe.py
@@ -9,3 +9,28 @@ class TestTickTackToe(unittest.TestCase):
         test = TickTackToe()
         self.assertEqual(test.next_turn("X"), "O")
         self.assertEqual(test.next_turn("O"), "X")
+
+    def test_new_board(self):
+        test = TickTackToe()
+        board = test.new_board()
+        for i in range(test.NUM_SQUARES):
+            self.assertEqual(board[i], test.EMPTY)
+
+    def test_winner(self):
+        test = TickTackToe()
+        board = test.new_board()
+        board[0] = board[1] = board[2] = test.player_x
+        self.assertEqual(test.winner(board), test.player_x)
+
+        board = test.new_board()
+        board[0] = board[4] = board[8] = test.player_o
+        self.assertEqual(test.winner(board), test.player_o)
+
+    def test_legal_moves(self):
+        test = TickTackToe()
+        board = test.new_board()
+        board[0] = board[4] = board[8] = test.player_o
+        board[1] = board[2] = board[3] = test.player_x
+
+        moves = test.legal_moves(board)
+        self.assertEqual(moves, [5, 6, 7])
